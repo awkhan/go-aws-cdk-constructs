66a91db8fee14f4a9bf46f7edb03cecbe98f0b30
Arbab Khan <arbabwkhan@gmail.com>
2023-11-19 15:16:03 -0500

    Added parsing configuration

---
+++ pkg/common/configuration.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/jsii-runtime-go"
+	"os"
+)
+
+func ParseConfigurationInto(app awscdk.App, cfg interface{}) {
+
+	env := app.Node().TryGetContext(jsii.String("environment"))
+	if env == nil {
+		fmt.Println("no environment supplied. Pass in as '-c environment=production|staging")
+		os.Exit(1)
+	}
+
+	m := app.Node().GetContext(jsii.String(env.(string))).(map[string]interface{})
+	data, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println("unable to parse configuration data")
+		os.Exit(2)
+	}
+
+	err = json.Unmarshal(data, &app)
+	if err != nil {
+		fmt.Println("unable to unmarshal configuration")
+		os.Exit(3)
+	}
+
+}
